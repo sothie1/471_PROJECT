ec0af68c1cbc6d10ed2ac30d3a1c0bd6265b46fc
Jamison Hyman <jhyman2@umbc.edu>
2013-11-15 14:08:59 -0500

    firstFunction

--- src/traveling_saleman/Traveling_saleman.java
+++ src/traveling_saleman/Traveling_saleman.java
@@ -1,6 +1,12 @@
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
+ * 
+ * 
+ * QUESTIONS FROM JAMISON:
+ * 1) What is the 2d array of coordinates storing? Why 2d?
+ * 2) Do we have to store the names of each thing?
+ * 
  */
 package traveling_saleman;
 
@@ -32,4 +38,53 @@ public class Traveling_saleman {
         
         JOptionPane.showMessageDialog(null,"Hello World");
     }
+    
+    // Takes in a list of coordinates left to travel to, and then 2 nulls
+    // Will recursively call self and change nulls to values
+    // Returns the arraylist of coordinates in order of "shortest path"
+    public ArrayList<Coordinate> getMin(ArrayList<Coordinate> remaining, ArrayList<Coordinate> path, 
+            Coordinate startAndEnd, Coordinate current){
+        
+        // make sure there's enough coordinates left
+        if (remaining.size() < 2){
+            path.add(startAndEnd);
+            return path;
+        }
+        
+        // should only happen once, when this function runs the first time
+        if (startAndEnd == null){
+            path.add(remaining.get(0));
+            current = remaining.get(0);
+            remaining.remove(0);
+        }
+        
+        // Finds shortest distance out of all coordinates
+        Coordinate temp = remaining.get(0);
+        int location = 0;
+        for (int i = 0; i < remaining.size()-1; i++){
+            if (current.getDistant(remaining.get(i)) < current.getDistant(remaining.get(i+1))){
+                temp = remaining.get(i);
+                location = i;
+            }
+        }
+        
+        // Checks for last element in list
+        if (current.getDistant(remaining.get(remaining.size())) < current.getDistant(temp)){
+            temp = remaining.get(remaining.size());
+            location = remaining.size();
+        }
+        
+        // must be done before calling again
+        path.add(temp);
+        current = temp;
+        remaining.remove(location);
+        
+        getMin(remaining, path, startAndEnd, current);
+        
+        // Does this happen more than once?  I forgot recursion.  I only want it
+        // to happen at the end when the path is complete.
+        return path;
+    }
+
+
 }
